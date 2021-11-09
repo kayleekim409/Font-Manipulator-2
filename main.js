e4e92bbfd2fc872edcdf16a1d01b080ca6d9280e
kayleekim409 <81453056+kayleekim409@users.noreply.github.com>
2021-11-08 16:48:02 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 function setup() {
     video = createCapture(VIDEO);
     video.size(300, 300);
-    poseNet.ml5.poseNet(video, modelLoaded);
+    var poseNet=ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
     canvas = createCanvas(550, 550);
     canvas.position(560, 150);
+}
+function gotPoses(results) {
+    if (length > 0) {
+        console.log(results);
+    }
+}
+function modelLoaded() {
+    console.log("Model is loaded");
 }
